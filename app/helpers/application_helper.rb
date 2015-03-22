d899da6e5860e578b7beae3ca03875d0e2287154
David Peterson <pete2786@umn.edu>
2015-03-22 09:21:06 -0500

    Added icons and cleaner signin prompting.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,10 @@ module ApplicationHelper
   end
 
   def signin_path
-    "/auth/facebook"
+    "/auth/facebook?origin=#{request.url}"
+  end
+
+  def link_to_or_siginin(name, path)
+    current_user ? link_to(name, path) : link_to(name, '#', "data-toggle": 'modal', "data-target": '#sign_in_modal')
   end
 end
