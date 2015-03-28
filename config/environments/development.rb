28df9c3237a033caae2468baeb7de7099751de25
David Peterson <pete2786@umn.edu>
2015-03-28 13:39:01 -0500

    Add email config w secrets.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,10 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,19 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.routes.default_url_options[:host] = 'http://mnweather.tandemdd.com'
+  Rails.application.routes.default_url_options[:host] = Rails.application.secrets.host
+  config.action_mailer.default_url_options = { :host => Rails.application.secrets.host }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+  config.action_mailer.smtp_settings = {
+    :address   => Rails.application.secrets.smtp_provider,
+    :port      => Rails.application.secrets.smtp_port,
+    :user_name => Rails.application.secrets.smtp_user,
+    :password  => Rails.application.secrets.smtp_pass
+  }
 end
