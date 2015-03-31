433aceb5f1ccd88fb9e141f34fa5f83b39463707
David Peterson <pete2786@umn.edu>
2015-03-30 21:38:50 -0500

    Give badge custome layout.:

--- app/controllers/badges_controller.rb
+++ app/controllers/badges_controller.rb
@@ -1,4 +1,5 @@
 class BadgesController < ApplicationController
+  layout false
   before_filter :find_user
   before_filter :check_user_badge
 
@@ -9,5 +10,9 @@ class BadgesController < ApplicationController
 
   def check_user_badge
     redirect_to user_path(@user) unless @user.has_badge?(params[:id].to_i)
+  end
+
+  def determine
+    params.keys.include?("modal") ?  false : 'application_bs'
   end
 end
