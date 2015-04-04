72bb0964a07e7554f408efdc5a5992ef4158cc9a
David Peterson <pete2786@umn.edu>
2015-04-04 11:06:57 -0500

    Fix api route.

--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :current_conditions do
-        get :forecast, on: :member
+        get :forecast
       end
     end
   end
